5a5dd02316b1622379391e0cecb74c0769c3c079
Witoo Harianto <witoo@outlook.com>
2013-10-04 16:52:48 +0700

    initialinitial

---
+++ src/Witooh/Utilities/Helper.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!function_exists('array_diff_recursive')) {
+    function array_diff_recursive($aArray1, $aArray2)
+    {
+        $aReturn = array();
+
+        foreach ($aArray1 as $mKey => $mValue) {
+            if (array_key_exists($mKey, $aArray2)) {
+                if (is_array($mValue)) {
+                    $aRecursiveDiff = array_diff_recursive($mValue, $aArray2[$mKey]);
+                    if (count($aRecursiveDiff)) {
+                        $aReturn[$mKey] = $aRecursiveDiff;
+                    }
+                } else {
+                    if ($mValue != $aArray2[$mKey]) {
+                        $aReturn[$mKey] = $mValue;
+                    }
+                }
+            } else {
+                $aReturn[$mKey] = $mValue;
+            }
+        }
+
+        return $aReturn;
+    }
+}
---
+++ src/Witooh/Utilities/UtilitiesServiceProvider.php
@@ -0,0 +1,39 @@
+<?php namespace Witooh\Utilities;
+
+use Illuminate\Support\ServiceProvider;
+
+class UtilitiesServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+    public function boot()
+    {
+        require_once('Helper.php');
+    }
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
